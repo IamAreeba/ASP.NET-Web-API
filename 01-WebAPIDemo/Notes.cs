8d277c65ee3727b532a065ed9bf121f35b9f8b1d
Areeba Amjad <areeba.amjad2020@gmail.com>
2025-10-04 12:28:10 +0500

    feat : 3 - HTTP GET PUT POST DELETE

---
+++ 01-WebAPIDemo/Notes.cs
@@ -0,0 +1,8 @@
+﻿
+// ==================================== 03 =========================================
+/*
+    Object: HTTP GET PUT POST DELETE
+    . For post, put and delete methods the return type is void and thast the reason we get the status code but we have complete control on
+      what we want to return
+    
+*/
