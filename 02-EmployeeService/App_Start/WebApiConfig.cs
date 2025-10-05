91aff3ccb6eea46d7ca3db5fac2393caa6d9bc12
Areeba Amjad <areeba.amjad2020@gmail.com>
2025-10-05 21:07:16 +0500

    feat : 3 - Content Negotiation

--- 02-EmployeeService/App_Start/WebApiConfig.cs
+++ 02-EmployeeService/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -19,6 +20,12 @@ namespace _02_EmployeeService
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Indent JSON Data
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+            // Camel Case instead Pascal Case
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
--- 02-EmployeeService/Notes.cs
+++ 02-EmployeeService/Notes.cs
@@ -4,3 +4,11 @@
     Object: Creating Web API service that returns data from a SQL Server DB
     
 */
+
+// ==================================== 03 =========================================
+/*
+    Object: Content Negotiation
+    . We want the json data to be in camel case
+    
+*/
+
